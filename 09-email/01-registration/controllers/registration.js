fba8179849d6590ac4aedb1a77a0533d56144034
Artem Lapkin <artemka666@mail.ru>
2020-12-13 21:35:27 +0300

    9 module wip

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -3,7 +3,15 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const userData = {
+    email: ctx.request.body.email,
+    displayName: ctx.request.body.displayName,
+    password: ctx.request.body.password,
+    verificationToken: uuid()
+  }
 
+  const user = await User.create(userData);
+  console.log(user);
 };
 
 module.exports.confirm = async (ctx, next) => {
